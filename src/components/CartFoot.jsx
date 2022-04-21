8b6d62f729939d4afb2fcdb3adef2820512834e2
Tomás Aranda <tomasdeveloperfrontend@gmail.com>
2022-04-21 01:29:37 -0300

    add responsive design

--- src/components/CartFoot.jsx
+++ src/components/CartFoot.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../css/cartfoot.css';
 import ProductCartFoot from './ProductCartFoot';
 
 const CartFoot = ({ clickCartFood, cartFood, deleteAmountProduct, filterProduct }) => {
+	const [total, setTotal] = useState(0);
+
+	useEffect(() => {
+		const priceTotal = cartFood.reduce((acc, current) => acc + current.amount * current.price, 0);
+		return setTotal(priceTotal);
+	}, [cartFood]);
+
+	let REMOVE_DIGIT = `${total}`.indexOf('.');
+
 	return (
 		<div className={clickCartFood ? 'cart-foot__appear' : 'cart-foot__hide'}>
 			<div className="cart-foot__container-title">
@@ -17,6 +26,10 @@ const CartFoot = ({ clickCartFood, cartFood, deleteAmountProduct, filterProduct 
 						filterProduct={filterProduct}
 					/>
 				))}
+			</div>
+			<div className="cart-foot__container-total">
+				<p>Total $ {`${total}`.slice(0, REMOVE_DIGIT + 3)} </p>
+				<button>To Pay</button>
 			</div>
 		</div>
 	);
